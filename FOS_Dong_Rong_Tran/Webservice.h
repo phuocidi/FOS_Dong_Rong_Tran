73ab6805b69ec5959f314480bc8e7d1bd9ec1d8a
phuocidi <phuocidi@gmail.com>
2016-12-30 13:39:01 -0600

    init Webservice

---
+++ FOS_Dong_Rong_Tran/Webservice.h
@@ -0,0 +1,13 @@
+//
+//  Webservice.h
+//  FOS_Dong_Rong_Tran
+//
+//  Created by Huu Tran on 12/30/16.
+//  Copyright © 2016 rjtcompuquest. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Webservice : NSObject
+
+@end
---
+++ FOS_Dong_Rong_Tran/Webservice.m
@@ -0,0 +1,13 @@
+//
+//  Webservice.m
+//  FOS_Dong_Rong_Tran
+//
+//  Created by Huu Tran on 12/30/16.
+//  Copyright © 2016 rjtcompuquest. All rights reserved.
+//
+
+#import "Webservice.h"
+
+@implementation Webservice
+
+@end
