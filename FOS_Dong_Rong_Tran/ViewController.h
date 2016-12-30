93c852294ef657d6c0387979816742dbad6f3068
phuocidi <phuocidi@gmail.com>
2016-12-30 13:14:36 -0600

    Initial Commit

---
+++ FOS_Dong_Rong_Tran/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FOS_Dong_Rong_Tran
+//
+//  Created by Huu Tran on 12/30/16.
+//  Copyright © 2016 rjtcompuquest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FOS_Dong_Rong_Tran/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FOS_Dong_Rong_Tran
+//
+//  Created by Huu Tran on 12/30/16.
+//  Copyright © 2016 rjtcompuquest. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
