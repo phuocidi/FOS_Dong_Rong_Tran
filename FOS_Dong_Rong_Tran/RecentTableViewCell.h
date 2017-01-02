e0f7b55785a63b1abd5e94956b66ea0e0a5af49c
YIFU RONG <yifu.rong@wmich.edu>
2017-01-02 00:03:40 -0500

    Add Recent Order Page

---
+++ FOS_Dong_Rong_Tran/RecentTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  RecentTableViewCell.h
+//  FOS_Dong_Rong_Tran
+//
+//  Created by YIFU RONG on 1/1/17.
+//  Copyright © 2017 rjtcompuquest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecentTableViewCell : UITableViewCell
+
+@end
---
+++ FOS_Dong_Rong_Tran/RecentTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  RecentTableViewCell.m
+//  FOS_Dong_Rong_Tran
+//
+//  Created by YIFU RONG on 1/1/17.
+//  Copyright © 2017 rjtcompuquest. All rights reserved.
+//
+
+#import "RecentTableViewCell.h"
+
+@implementation RecentTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
